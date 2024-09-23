9f48372e7e47832476a01598b9c4f1bf9e557000
StevenATDev <steven8724@gmail.com>
2024-09-22 22:08:45 -0500

    Todos los desafíos funcionando

--- Traductor/src/app/components/desafios/desafio-five/desafio-five.page.ts
+++ Traductor/src/app/components/desafios/desafio-five/desafio-five.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, NavController } from '@ionic/angular';
+import { UsersService } from 'src/app/service/users.service';
 
 @Component({
   selector: 'app-desafio-five',
@@ -11,13 +12,91 @@ import { IonicModule, NavController } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class DesafioFivePage implements OnInit {
+  id: string = '';
+  mostrarVentana: boolean = false;
+  // Aquí se guardan las respuestas del usuario
+  respuestas = {
+    pregunta1: '',
+    pregunta2: '',
+    pregunta3: '',
+    pregunta4: '',
+    pregunta5: '',
+    pregunta6: '',
+    pregunta7: '',
+    pregunta8: '',
+    pregunta9: '',
+    pregunta10: ''
+  };
 
-  constructor(private _navCtrl: NavController) { }
+  // Respuestas correctas para comparar
+  correctas = {
+    pregunta1: 'sankantiai',
+    pregunta2: 'yakatiai',
+    pregunta3: 'weshan',
+    pregunta4: 'waatai',
+    pregunta5: 'kunkuimiach',
+    pregunta6: 'weawea',
+    pregunta7: 'kunkuimiach',
+    pregunta8: 'waatai',
+    pregunta9: 'wesham',
+    pregunta10: 'weawea'
+  };
+
+  puntaje = 0;
+
+  constructor(private _usersService: UsersService, private _navCtrl: NavController) {
+    this._usersService.getSession('id').then((data: any) => {
+      this.id = data;
+    })
+  }
 
   ngOnInit() {
   }
 
+  enviarRespuestas() {
+    this.puntaje = 0; // Reiniciar el puntaje
+
+    // Comparar las respuestas del usuario con las correctas
+    if (this.respuestas.pregunta1 === this.correctas.pregunta1) this.puntaje++;
+    if (this.respuestas.pregunta2 === this.correctas.pregunta2) this.puntaje++;
+    if (this.respuestas.pregunta3 === this.correctas.pregunta3) this.puntaje++;
+    if (this.respuestas.pregunta4 === this.correctas.pregunta4) this.puntaje++;
+    if (this.respuestas.pregunta5 === this.correctas.pregunta5) this.puntaje++;
+    if (this.respuestas.pregunta6 === this.correctas.pregunta6) this.puntaje++;
+    if (this.respuestas.pregunta7 === this.correctas.pregunta7) this.puntaje++;
+    if (this.respuestas.pregunta8 === this.correctas.pregunta8) this.puntaje++;
+    if (this.respuestas.pregunta9 === this.correctas.pregunta9) this.puntaje++;
+    if (this.respuestas.pregunta10 === this.correctas.pregunta10) this.puntaje++;
+
+    console.log('Puntaje obtenido: ', this.puntaje);
+
+    this.mostrarVentana = true;
+
+    this.guardarPuntaje();
+  }
+
+  guardarPuntaje() {
+    let datos = {
+      accion: 'guardarPuntaje',
+      usuario_id: this.id,
+      puntaje: this.puntaje,
+      desafio: 5
+    }
+    this._usersService.postData(datos).subscribe((data: any) => {
+      if (data.estado == true) {
+        console.log(data.mensaje);
+      } else {
+        console.log(data.mensaje);
+      }
+    })
+  }
+
   goBack() {
-    this._navCtrl.back();
+    window.history.back();
+  }
+
+  cerrarVentana() {
+    this.mostrarVentana = false;
+    this._navCtrl.navigateRoot(['/lecciones']);
   }
 }
