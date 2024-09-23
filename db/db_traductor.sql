f309c93b87c0d2bd1db7ada2bed863acb47820e9
StevenATDev <steven8724@gmail.com>
2024-09-22 22:43:55 -0500

    Bases de datos actualizada

--- db/db_traductor.sql
+++ db/db_traductor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-09-2024 a las 21:27:04
+-- Tiempo de generación: 23-09-2024 a las 05:43:27
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -36,7 +36,34 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `cedula`, `nombre`, `apellido`, `correo`, `telefono`, `password`) VALUES
-(1, '0803563774', 'Alexander Steven', 'Torres Santa', 'stev74@gmail.com', '0967201167', '$2y$10$wRKDPSpqHv6rj3WQ1Hrg.ucvqBMORDE53.TSaXkeAGQihspvJ8/ZK');
+(1, '0803563774', 'Alexander Steven', 'Torres Santa', 'steven@gmail.com', '0967201167', '$2y$10$dIz7h49ghSV1vWaGVhcFOOUj2pt72LLjirVvqRLVYD0YZ9Vf7lIQW');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puntajes`
+--
+
+CREATE TABLE `puntajes` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `puntaje` int(11) NOT NULL,
+  `desafio` int(11) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `puntajes`
+--
+
+INSERT INTO `puntajes` (`id`, `usuario_id`, `puntaje`, `desafio`, `fecha`) VALUES
+(1, 9, 9, 1, '2024-09-23 00:29:52'),
+(2, 9, 8, 1, '2024-09-23 00:32:46'),
+(3, 9, 6, 1, '2024-09-23 00:34:03'),
+(4, 9, 10, 2, '2024-09-23 01:16:36'),
+(5, 9, 10, 3, '2024-09-23 01:50:26'),
+(6, 9, 7, 4, '2024-09-23 02:31:53'),
+(7, 9, 9, 5, '2024-09-23 03:06:46');
 
 -- --------------------------------------------------------
 
@@ -48,22 +75,53 @@ CREATE TABLE `translations` (
   `id` int(11) NOT NULL,
   `shuar` varchar(255) NOT NULL,
   `espanol` varchar(255) NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
+  `audio` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Volcado de datos para la tabla `translations`
 --
 
-INSERT INTO `translations` (`id`, `shuar`, `espanol`, `createdAt`, `updatedAt`) VALUES
-(1, 'tsatsaka', 'somos', '2024-08-31 21:33:20', '2024-08-31 21:33:20'),
-(2, 'yuku', 'agua', '2024-08-31 21:33:20', '2024-08-31 21:33:20'),
-(3, 'Kawsayki, kikinka imashina?', 'Hola, ¿cómo estás?', '2024-09-01 01:21:22', '2024-09-01 01:21:22'),
-(4, 'Ñukaka Juanmi.', 'Me llamo Juan.', '2024-09-01 01:21:22', '2024-09-01 01:21:22'),
-(5, 'Maylli kawsak?', '¿Dónde está el baño?', '2024-09-01 01:21:22', '2024-09-01 01:21:22'),
-(6, 'Yurakuk aña yachana yuyarichik.', 'Gracias por tu ayuda.', '2024-09-01 01:21:22', '2024-09-01 01:21:22'),
-(7, 'Ñukaka shuk killa?', '¿Cuántos años tienes?', '2024-09-01 01:21:22', '2024-09-01 01:21:22');
+INSERT INTO `translations` (`id`, `shuar`, `espanol`, `audio`) VALUES
+(1, 'tsatsaka', 'somos', ''),
+(2, 'yuku', 'agua', ''),
+(3, 'Kawsayki, kikinka imashina?', 'Hola, ¿cómo estás?', ''),
+(4, 'Ñukaka Juanmi.', 'Me llamo Juan.', ''),
+(5, 'Maylli kawsak?', '¿Dónde está el baño?', ''),
+(6, 'Yurakuk aña yachana yuyarichik.', 'Gracias por tu ayuda.', ''),
+(7, 'Ñukaka shuk killa?', '¿cuántos años tienes?', ''),
+(8, 'apa', 'papá', ''),
+(9, 'nuku', 'mamá', ''),
+(10, 'kapaku', 'rojo', ''),
+(11, 'kinkla', 'azul', ''),
+(12, 'kinkla', 'azul', ''),
+(13, 'samék', 'verde', ''),
+(14, 'yumkumá', 'amarillo', ''),
+(15, 'chikichik', 'uno', ''),
+(16, 'jímiar', 'dos', ''),
+(17, 'mehaínt', 'tres', ''),
+(18, 'aintiuk', 'cuatro', ''),
+(19, 'uchi', 'hijo', ''),
+(20, 'nawánt', 'hija', ''),
+(21, 'apachich´', 'abuelo', ''),
+(22, 'nukuchich´', 'abuela', ''),
+(23, 'sankántiai', 'pelota', ''),
+(24, 'yakátai', 'lapices de colores', ''),
+(25, 'waátai', 'rompecabezas', ''),
+(26, 'kunkuimiach', 'carrito', ''),
+(27, 'wea-wea', 'trompo', ''),
+(28, 'yawá', 'perro', ''),
+(29, 'michik', 'gato', ''),
+(30, 'chai', 'oso', ''),
+(31, 'uunt yawá', 'tigre', ''),
+(32, 'yantána', 'tigrillo', ''),
+(33, 'pátu', 'pato', ''),
+(34, 'traducir', 'hola', ''),
+(35, 'traducir', 'buenos días', ''),
+(36, 'traducir', 'buenas tardes', ''),
+(37, 'traducir', 'buenas noches', ''),
+(38, 'traducir', '¿cómo estás?', ''),
+(39, 'traducir', 'adiós', '');
 
 -- --------------------------------------------------------
 
@@ -89,9 +147,9 @@ INSERT INTO `users` (`id`, `cedula`, `nombre`, `apellido`, `correo`, `telefono`,
 (1, '0807675434', 'Manuel', 'Caicedo', 'caice@gmail.com', '', '$2b$10$hqLw7MUArm6CvGRW4wVEAe0Uny4nkHgNbT.JgnLiB0QbDsGE2/Txm'),
 (2, '0845234152', 'Alexander', 'Torres', 'alex@gmail.com', '098987675', '$2b$10$1rQtZ81W8bzvbJL9s5lKWe9J3ZkIl/hnAFvRHM.8gylOR1D3diBKS'),
 (3, '0876787675', 'Amelia', 'Aguilar', 'amelia@gmail.com', '0924536452', '$2b$10$RXvq1hxSIE78iiLo1QN8c.N76flgrTqmwDL9efiUz9WYLC092yev6'),
-(6, '0803563774', 'Alexander Steven', 'Torres Santa', 'stev74@gmail.com', '0967201167', '$2y$10$5H6SeB6zP.fOp5g1Xm7Zr./Qix1n3FhtUJPXuO6i6RtLuBf8r47xq'),
-(9, '1400373583', 'Ayuy', 'asdasdas', 'asdasdasd', '13123131', '$2y$10$YIooECs4hkmwRLvEZ1uLROEbHaG1KecAIkRvteqsDsNVcF12E/UqG'),
-(10, '2350017170', 'Josue', 'Caicedp', 'caicedo@gmail.com', '0967234567', '$2y$10$JnmvuKWbrVYunH4BGcc95eHmhPNkoLnqTPk3M2/S73aFMJ04q7vxq');
+(6, '0803563774', 'Alexander Steven', 'Torres Santa', 'steven74@gmail.com', '0967201167', '$2y$10$7egcmuGZoqGH2hvDRPSD..4J88sprbGObSGVFRbLjGDAqJvhmJjza'),
+(9, '1400373583', 'Wilmer', 'Ayuy', 'wilmer@gmail.com', '0989876567', '$2y$10$aHqY1KGAIGzKszR1GvIZX.JEd9IdlCY8I7Boql41o1hrDQLJOtFwK'),
+(10, '2350017170', 'Carlos', 'Caicedo', 'caicedo@gmail.com', '0967234567', '$2y$10$uZv2aWzBwS.Ob1dj0ATHKexi2TKN1UBFmUfS2G8v2ERCwit7LMB5a');
 
 --
 -- Índices para tablas volcadas
@@ -101,6 +159,12 @@ INSERT INTO `users` (`id`, `cedula`, `nombre`, `apellido`, `correo`, `telefono`,
 -- Indices de la tabla `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `puntajes`
+--
+ALTER TABLE `puntajes`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -123,13 +187,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `puntajes`
+--
+ALTER TABLE `puntajes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `translations`
 --
 ALTER TABLE `translations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
